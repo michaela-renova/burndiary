9cbcf50e7d8002ba6ac82983844f55bdf38088c2
michaela-renova <renovamichaela@gmail.com>
2025-11-24 18:06:07 +0100

    final edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 <div class="mx-auto">
     <h1 class="font-bold text-orange-500 text-3xl mb-6">Moje příspěvky</h1>
     <div class ="grid grid-cols-1 lg:grid-cols-2 gap-6">
-    @foreach ($posts as $post)
+    @forelse ($posts as $post)
             <div class="p-6 bg-white rounded-2xl shadow-md">
                 <h2 class="text-xl font-semibold mb-2">{{ $post['title'] }}</h2>
                 <div class="text-gray-700 mb-4">
@@ -40,8 +40,11 @@
         </div>
 
 
-    @endforeach
-    </div>
+@empty
+    <p class="text-sm text-gray-400">Žádné příspěvky</p>
+@endforelse
+
+</div>
 </div>
 
 
